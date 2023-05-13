42ab636f0a598a78009652fc29a1ee5d82dec8cf
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-05-13 10:34:16 +0530

    redux store setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { Provider } from "react-redux";
+
+import App from "./App";
+import { store } from "./services/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
---
+++ src/services/article.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const rapidapiKey = import.meta.env.VITE_RAPIDAPI_ARTICLE_KEY;
+
+export const articleApi = createApi({
+  reducerPath: "articleApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
+    prepareHeaders: (headers) => {
+      headers.set("X-RapidAPI-Key", rapidapiKey);
+      headers.set(
+        "X-RapidAPI-Host",
+        "article-extractor-and-summarizer.p.rapidapi.com"
+      );
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getSummary: builder.query({
+      query: (params) => `test`,
+    }),
+  }),
+});
