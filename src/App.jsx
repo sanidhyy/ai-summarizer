318f55cdb5dd9bf0f3a417a224f458f9ac9b0046
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-05-13 09:38:36 +0530

    Tailwind CSS Setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-center text-green-500 text-3xl">
+        OpenAI Article Summarizer
+      </h1>
+    </div>
+  );
+};
+
+export default App;
