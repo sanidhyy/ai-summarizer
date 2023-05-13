c5a7e0916e5b70f7f762a4b47d6eed63823c886c
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-05-13 09:52:15 +0530

    Hero section done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,37 @@
 import React from "react";
 
+import { logo } from "../assets";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="Sumz" className="w-28 object-contain" />
+
+        <button
+          type="button"
+          onClick={() =>
+            window.open("https://github.com/Technical-Shubham-tech", "_blank")
+          }
+          className="black_btn"
+        >
+          Github
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+
+      <h2 className="desc">
+        Simplify your reading with{" "}
+        <span className="orange_gradient">Summize</span>, an open-source article
+        summarizer that transforms lengthy articles into clear and concise
+        summaries
+      </h2>
+    </header>
+  );
 };
 
 export default Hero;
